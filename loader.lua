47196deaef76980f11de51b0b56450aa7a00f436
Zanund <zanund.gd@gmail.com>
2026-03-16 15:32:53 +0500

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,10 @@
+local Games = {
+    [123456789] = "https://raw.githubusercontent.com/USERNAME/REPO/main/game1.lua",
+    [987654321] = "https://raw.githubusercontent.com/USERNAME/REPO/main/game2.lua"
+}
+
+local universal = "https://raw.githubusercontent.com/USERNAME/REPO/main/universal.lua"
+
+local url = Games[game.PlaceId] or universal
+
+loadstring(game:HttpGet(url))()

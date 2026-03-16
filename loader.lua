e92c0ac52a8dc37408ffa01ec7b0c2696a3217ce
Zanund <zanund.gd@gmail.com>
2026-03-16 15:35:17 +0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 local Games = {
-    [123456789] = "https://raw.githubusercontent.com/Zanund/Zankuru-Hub/refs/heads/main/PlantsVsBrainrots.lua",
+    [127742093697776] = "https://raw.githubusercontent.com/Zanund/Zankuru-Hub/refs/heads/main/PlantsVsBrainrots.lua",
     [987654321] = "https://raw.githubusercontent.com/USERNAME/REPO/main/game2.lua"
 }
 

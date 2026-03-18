c975875cc5b38190d04eece9a55f3223e349469f
Zanund <zanund.gd@gmail.com>
2026-03-18 16:45:36 +0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 local Games = {
     [127742093697776] = "https://raw.githubusercontent.com/Zanund/Zankuru-Hub/refs/heads/main/PlantsVsBrainrots.lua",
     [1537690962] = "https://raw.githubusercontent.com/Zanund/Zankuru-Hub/refs/heads/main/Bee%20Swarm%20Simulator.lua",
-    [137925884276740] = "https://raw.githubusercontent.com/Zanund/Zankuru-Hub/refs/heads/main/Bee%20Swarm%20Simulator.lua"
+    [137925884276740] = "https://raw.githubusercontent.com/Zanund/Zankuru-Hub/refs/heads/main/Build%20a%20Plane.lua"
 }
 
 local universal = "https://raw.githubusercontent.com/USERNAME/REPO/main/universal.lua"
